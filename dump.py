1a9947ed52ab390b82937785f922db339490b381
Ronen Haber <ronhab@gmail.com>
2019-06-05 10:07:26 +0300

    read database settings from config

--- dump.py
+++ dump.py
@@ -1,6 +1,7 @@
 import psycopg2
 import psycopg2.extensions
 import numpy as np
+import json
 
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
@@ -44,7 +45,9 @@ if __name__ == "__main__":
         where c.type != 'blamed_commit'
         limit 800;
     """
-    data = Data("dbname=postgres host=localhost port=55432 user=postgres")
+    with open("var/db_config.json", "r") as config_file:
+        json_config = json.loads(config_file.read())
+    data = Data("dbname={0} host={1} port={2} user={3}".format(json_config["dbname"], json_config["host"], json_config["port"], json_config["user"]))
     # rows = data.fetch(query_all)
     # np.savez_compressed("var/vcc_data.npz", np.array(rows))
     vcc = data.fetch(query_vulnerable)
